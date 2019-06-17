24258dcef00c396565a01211122b6b7d3e9cfa33
scott <scott@sixnology.com>
2019-06-18 00:18:12 +0800

    Adjust description

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,14 @@ from scripts.auto_g0v_hackmd import G0V_HACK_MD_Script
 from scripts.auto_kktix import VTW_KKTIX_Script
 
 hackMdScript = G0V_HACK_MD_Script()
-url = hackMdScript.run()
-vtwScript = VTW_KKTIX_Script(url)
-vtwScript.run()
+hackMdUrl = hackMdScript.run()
+vtwScript = VTW_KKTIX_Script(hackMdUrl)
+kktixUrl = vtwScript.run()
 
+print hackMdUrl
+print kktixUrl
 
+# TODO:
+    # Post on Facebook
+    # Post on Slack
+    # Post on vtw.link
