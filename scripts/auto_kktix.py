df861fc547a97cec8e8a2ff4f4979f34cf458582
scott <scott@sixnology.com>
2019-06-17 23:12:39 +0800

    Rename field, also adjust delay and complete whole process

--- scripts/auto_kktix.py
+++ scripts/auto_kktix.py
@@ -28,22 +28,26 @@ class VTW_KKTIX_Script:
         self.driver.find_element_by_id("user_password").send_keys(account.kktix_password)
         time.sleep(self.SHORT_DELAY)
         self.driver.find_element_by_name("commit").click()
-        time.sleep(self.LONG_DELAY)
+        time.sleep(self.SHORT_DELAY)
 
     def goOpenActivity(self):
         self.driver.get('https://kktix.com/dashboard/events/new?organization=vtaiwan')
-        time.sleep(self.LONG_DELAY)
+        time.sleep(self.SHORT_DELAY)
 
     def chooseEmptyTemplate(self):
         elements = self.driver.find_elements_by_class_name("radio")
         elements[0].click()
         time.sleep(self.SHORT_DELAY)
         self.driver.find_element_by_class_name("btn-primary").click()
-        time.sleep(self.LONG_DELAY)
+        time.sleep(self.SHORT_DELAY)
 
     def setTitle(self):
         self.driver.find_element_by_id("event_name").send_keys(kktix_event_content.title.decode('utf8'))
-        time.sleep(self.NORMAL_DELAY)
+        time.sleep(self.SHORT_DELAY)
+
+    def storeUrl(self):
+        self.url = "https://vtaiwan.kktix.cc/events/" + self.driver.find_element_by_name("event[slug]").get_attribute("value")
+        print self.url
 
     def setTime(self):
         futureDay = FutureDay()
@@ -57,6 +61,11 @@ class VTW_KKTIX_Script:
         dateElements[0].click()
         time.sleep(self.SHORT_DELAY)
 
+        # 保險起見，先點擊當天的日期，再按下右鍵
+        self.driver.find_element_by_css_selector("body > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-bottom > div.datepicker-days > table > tfoot > tr:nth-child(1) > th").click()
+        dateElements[0].click()
+
+        time.sleep(self.SHORT_DELAY)
         for i in range(daysAhead):
             dateElements[0].send_keys(Keys.RIGHT)
 
@@ -65,7 +74,7 @@ class VTW_KKTIX_Script:
         timeElements[0].send_keys(Keys.BACK_SPACE)
         timeElements[0].send_keys("19:00")
         timeElements[0].send_keys(Keys.ENTER)
-        time.sleep(self.NORMAL_DELAY)
+        time.sleep(self.SHORT_DELAY)
 
         timeElements[0].send_keys(Keys.TAB)
         dateElements[1].send_keys(next_wednesday)
@@ -73,40 +82,61 @@ class VTW_KKTIX_Script:
         timeElements[1].send_keys(Keys.BACK_SPACE)
         timeElements[1].send_keys("21:00")
         timeElements[1].send_keys(Keys.ENTER)
-        time.sleep(self.NORMAL_DELAY)
+        time.sleep(self.SHORT_DELAY)
 
     def setMetaData(self):
         self.driver.find_element_by_id("event_capacity").clear()
         self.driver.find_element_by_id("event_capacity").send_keys("15")
         self.driver.find_element_by_id("event_location").send_keys("社會創新實驗中心(2樓A9會議室)".decode('utf8'))
         self.driver.find_element_by_id("event_location_address").send_keys("台北市大安區仁愛路三段99號".decode('utf8'))
-        time.sleep(self.NORMAL_DELAY)
+        time.sleep(self.SHORT_DELAY)
 
     def setDescription(self):
         self.driver.find_element_by_class_name("cke_button__source").click()
-        time.sleep(self.NORMAL_DELAY)
+        time.sleep(self.SHORT_DELAY)
         body_string =kktix_event_content.description.format(self.hackMdUrl, self.hackMdUrl)
         self.driver.find_element_by_class_name("cke_contents_ltr").send_keys((body_string).decode('utf8'))
+        time.sleep(self.SHORT_DELAY)
+
+    def goTicketSetting(self):
+        self.driver.find_element_by_class_name("btn-primary").click()
+        time.sleep(self.SHORT_DELAY)
+
+    def goFormSetting(self):
+        self.driver.find_element_by_class_name("btn-primary").click()
+        time.sleep(self.SHORT_DELAY)
+
+    def disableCheckBoxPhone(self):
+        self.driver.find_element_by_css_selector("#new-event > div > div:nth-child(4) > div > div.contact-block.ng-scope > div > div.col-4 > table > tbody > tr:nth-child(3) > td > label > input").click()
         time.sleep(self.LONG_DELAY)
 
-    def run(self):
+    def clickConfirm(self):
+        self.driver.find_element_by_class_name("btn btn-primary").click()
+
+    def run(self, debugMode = False):
         try:
             self.go()
             self.loginKKTIX()
             self.goOpenActivity()
             self.chooseEmptyTemplate()
             self.setTitle()
+            self.storeUrl()
             self.setTime()
             self.setMetaData()
             self.setDescription()
+            self.goTicketSetting()
+            self.goFormSetting()
+            self.disableCheckBoxPhone()
+            if not debugMode:
+                self.clickConfirm()
+
         except Exception as e:
             print e
         finally:
             self.driver.close()
-            return
+            return self.url
 
 if __name__ == "__main__":
-    script = VTW_KKTIX_Script()
+    script = VTW_KKTIX_Script(True)
     script.run()
-
 
