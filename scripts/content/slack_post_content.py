da3522d4e5e9e8302a500b75d76e3bbbfad771d4
scott <scott@sixnology.com>
2019-08-05 21:58:41 +0800

    Change content

--- scripts/content/slack_post_content.py
+++ scripts/content/slack_post_content.py
@@ -2,7 +2,7 @@
 from scripts.utils.future_day import FutureDay
 
 bot_name = "自動小松機器人"
-title_text = "本週小松報名：\n歡迎直接編輯共筆～\n{0}\n{1}"
+title_text = "本次小松報名：\n歡迎直接編輯共筆～\n{0}\n{1}"
 
 future_day = FutureDay()
 wednesday_offset = 2
