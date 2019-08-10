0d77e924c87f5aa7bd9087f41f84249d6572bc24
scott <scott@sixnology.com>
2019-08-10 18:59:51 +0800

    disable kktix debug

--- scripts/main.py
+++ scripts/main.py
@@ -12,7 +12,7 @@ def timed_job():
     hack_md_script = G0vHackMdScript()
     hack_md_url = hack_md_script.run()
     vtw_script = VtwKKTixScript(hack_md_url)
-    kktix_url = vtw_script.run(True)
+    kktix_url = vtw_script.run()
     print hack_md_url
     print kktix_url
 
