5b1fe84990e327b6383dc09109c3c4c28269cc7d
scott <scott@sixnology.com>
2019-08-14 20:20:33 +0800

    Add jitsi link

--- scripts/content/slack_post_content.py
+++ scripts/content/slack_post_content.py
@@ -2,7 +2,7 @@
 from scripts.utils.future_day import FutureDay
 
 bot_name = "自動小松機器人"
-title_text = "本次小松報名：\n歡迎直接編輯共筆～\n{0}\n{1}"
+title_text = "本次小松報名：\n歡迎直接編輯共筆～\n{0}\n{1}\n{2}"
 
 future_day = FutureDay()
 wednesday_offset = 2
@@ -14,3 +14,7 @@ kktix_thumb_url = "https://t.kfs.io/assets/kktix-og-image-0b30b8444017c8a56ab631
 
 hack_md_title = "HackMD共筆({0})".format(next_wednesday)
 hack_md_thumb_url = "https://g0v.hackmd.io/favicon.png"
+
+jitsi_title = "直播連結"
+jitsi_link = "https://meet.jit.si/vtaiwan"
+jitsi_md_thumb_url = "https://slack-imgs.com/?c=1&o1=wi32.he32.si&url=https%3A%2F%2Fmeet.jit.si%2Fimages%2Ffavicon.ico%3Fv%3D1"
