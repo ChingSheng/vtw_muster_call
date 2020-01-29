9a357b606e8cb23633f84b274790c66e3c5f2629
scott <scott@sixnology.com>
2020-01-29 23:56:49 +0800

    Change back time

--- scripts/main.py
+++ scripts/main.py
@@ -11,16 +11,16 @@ def timed_job():
     print 'This job is run every three minutes.'
     hack_md_script = G0vHackMdScript()
     hack_md_url = hack_md_script.run()
+    print hack_md_url
     vtw_script = VtwKKTixScript(hack_md_url)
     kktix_url = vtw_script.run()
-    print hack_md_url
     print kktix_url
 
     slack_bot = AutoSlackBot(kktix_url, hack_md_url)
     slack_bot.run()
 
 sched = BlockingScheduler(timezone='Asia/Taipei')
-sched.add_job(timed_job, 'cron', day_of_week='wed', hour=23, minute=50)
+sched.add_job(timed_job, 'cron', day_of_week='wed', hour=21, minute=30)
 sched.start()
 
 # TODO:
